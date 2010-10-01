a1a5a1782f6a2ec33122914dd90f37934b46e52c
Scott Nelson <scottbnel@gmail.com>
2010-10-01 16:12:52 -0400

    Added type processing

--- earle.py
+++ earle.py
@@ -1,24 +1,15 @@
 import re
-
 from django.conf import settings
-from django.conf.urls.defaults import url
 from django.core.urlresolvers import RegexURLPattern
+from types import defaults, RegexEarlePattern, RegexEarleResolver
 
 re_template = r'<(?P<name>[a-zA-Z_][a-zA-Z_0-9]*):(?P<type>[a-z]+)>'
 re_pattern = re.compile(re_template)
 
-default_types = {
-    'alpha': '[a-zA-Z]+',
-    'alphanumeric': '[a-zA-Z0-9]+',
-    'any': '[^/]+',
-    'float': '\d*\.?\d+',
-    'int': '\d+',
-}
-
 class Earle(object):
-    
+
     def __init__(self):
-        self.types = dict([(k, v) for k, v in default_types.iteritems()])
+        self.types = dict([(k, v) for k, v in defaults.iteritems()])
         custom_types = getattr(settings, 'REGEX_TYPES', False)
         if custom_types:
             self.types.update(custom_types)
@@ -29,39 +20,66 @@ class Earle(object):
         for t in args:
             if isinstance(t, (list, tuple)):
                 t = self.url(prefix = prefix, *t)
-            elif isinstance(t, RegexURLPattern):
+            elif isinstance(t, (RegexEarlePattern, RegexURLPattern)):
                 t.add_prefix(prefix)
             pattern_list.append(t)
         return pattern_list
-    
+        
     def url(self, regex, view, kwargs = None, name = None, prefix = ''):
         '''Replacement for django.conf.urls.defaults.url.'''
-        regex = self.replace(regex)
-        regex = self.prepend(regex)
-        if not isinstance(view, (tuple, list)):
-            regex = self.append(regex)
-        return url(regex, view, kwargs, name, prefix)
+        regex, type_processors = self.replace_all(regex)
+        regex = self.prepend_caret(regex)
+        if isinstance(view, (list, tuple)):
+            module, app_name, namespace = view
+            return RegexEarleResolver(regex, type_processors, module, kwargs, app_name, namespace)
+        else:
+            regex = self.append_dollar(regex)
+            if isinstance(view, basestring):
+                if not view:
+                    raise ImproperlyConfigured('Empty URL pattern view name not permitted (for pattern %r)' % regex)
+                if prefix:
+                    view = prefix + '.' + view
+            return RegexEarlePattern(regex, type_processors, view, kwargs, name)
             
-    def replace(self, regex):
-        '''Replace Earle groups with normal regex groups.'''
+    def replace_all(self, regex):
+        '''Replace all Earle groups with normal regex groups and extract any type processors.'''
         matches = re_pattern.finditer(regex)
+        type_processors = {}
         for match in matches:
-            groups = match.groupdict()
-            replacement = r'(?P<%s>%s)' % (groups['name'], self.types[groups['type']])
-            start, end = match.span()
-            regex = regex[:start] + replacement + regex[end:]
-        return regex
+            group_name, group_type, group_type_processors = self.group(match)
+            type_processors.update(group_type_processors)
+            regex = self.replace_group(regex, group_name, group_type, match.span())
+        return regex, type_processors
+        
+    def group(self, match):
+        '''Extract group name, type, and type processors from a match.'''
+        groups = match.groupdict()
+        group_name = groups['name']
+        group_type = self.types[groups['type']]
+        type_processors = {}
+        if isinstance(group_type, (list, tuple)):
+            group_type, type_processor = group_type
+            type_processors[group_name] = type_processor
+        return group_name, group_type, type_processors
+        
+    def replace_group(self, regex, group_name, group_type, bounds):
+        '''Replace Earle group with normal regex group.'''
+        replacement = r'(?P<%s>%s)' % (group_name, group_type)
+        start, end = bounds
+        return regex[:start] + replacement + regex[end:]
             
-    def append(self, regex):
+    def append_dollar(self, regex):
         '''Append the regex with $ if not present.'''
         if not regex.endswith('$'):
             regex = regex + '$'
         return regex
             
-    def prepend(self, regex):
+    def prepend_caret(self, regex):
         '''Prepend the regex with ^ if not present.'''
         if not regex.startswith('^'):
             regex = '^' + regex
         return regex
 
-patterns = Earle().patterns
+earle = Earle()
+patterns = earle.patterns
+url = earle.url
---
+++ types.py
@@ -0,0 +1,41 @@
+from django.core.urlresolvers import RegexURLPattern, RegexURLResolver
+
+defaults = {
+    'alpha': '[a-zA-Z]+',
+    'alphanumeric': '[a-zA-Z0-9]+',
+    'any': '[^/]+',
+    'float': ('\d*\.?\d+', lambda x: float(x)),
+    'int': ('\d+', lambda x: int(x)),
+}
+
+class TypeProcessor(object):
+
+    def process(self, callback, args, kwargs):
+        '''Run any type processors on kwargs.'''
+        for name, value in kwargs.iteritems():
+            if name in self.type_processors:
+                kwargs[name] = self.type_processors[name](value)
+        return callback, args, kwargs
+
+class RegexEarlePattern(RegexURLPattern, TypeProcessor):
+    
+    def __init__(self, regex, type_processors, callback, default_args = None, name = None):
+        super(RegexEarlePattern, self).__init__(regex, callback, default_args = default_args, name = name)
+        self.type_processors = type_processors
+    
+    def resolve(self, path):
+        values = super(RegexEarlePattern, self).resolve(path)
+        if values:
+            return self.process(*values)
+
+        
+class RegexEarleResolver(RegexURLResolver, TypeProcessor):
+    
+    def __init__(self, regex, type_processors, module, kwargs, app_name, namespace):
+        super(RegexEarleResolver, self).__init(regex, module, kwargs, app_name, namespace)
+        self.type_processors = type_processors
+    
+    def resolve(self, path):
+        values = super(RegexEarleResolver, self).resolve(path)
+        if values:
+            return self.process(*values)
